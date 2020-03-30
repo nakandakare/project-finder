6d077665fe909f6ef41f8495cdca431240a089ee
Kevin <kevin.nakandakare@gmail.com>
2020-03-30 17:32:10 -0300

    added header

---
+++ project-finder/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Header from '../../components/Header/header.component';
+
+const Homepage = () => {
+    return (
+        <Header />
+    )
+}
+
+export default Homepage;
